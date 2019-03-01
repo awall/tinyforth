b8839184606267c0cf1141696e1e4b8088f0bc02
Adam Wall <agcwall@gmail.com>
2019-03-01 00:36:42 -0700

    add (+) and (..)

--- main.c
+++ main.c
@@ -2,68 +2,178 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define STRING_CAPACITY 16
-#define STRING_MAX_COUNT 1024
-#define WORD_CAPACITY 32
-#define WORD_MAX_COUNT 1024
-
-typedef size_t cell_t;
-
-typedef struct {
-	char* chars;
-} string_t;
+#define STRING_SIZE 16
+#define STRING_CAPACITY (STRING_SIZE - 1)
+#define MAX_CELLS_IN_DEFINITION 16
+#define MAX_DEFINITIONS_IN_DICTIONARY 1024
+#define MAX_STACK_SIZE 1024 
 
 typedef enum {
-	ERROR_READ_INPUT = 101,
-	ERROR_STRING_MAX_COUNT = 102,
-	ERROR_STRING_CAPACITY = 103
-} failure_t;
+	CONSTANT = 0,
+	BEGIN_DEFINE = 1,
+	END_DEFINE = 2,
+	TO_RETURN_STACK = 3,
+	FROM_RETURN_STACK = 4,
+	PRINT_STACK = 5,
+	PEEK_STACK = 6,
+	PLUS = 7
+} command_t;
 
-char g_string_heap[STRING_CAPACITY * STRING_MAX_COUNT];
-int g_string_count;
+typedef enum {
+	EXECUTE = 0,
+	COMPILE = 1
+} run_mode_t;
 
-void fail(failure_t failure) {
-	switch (failure) {
-		case ERROR_READ_INPUT:			puts("ERROR: failed to read input stream.\n");		break;
-		case ERROR_STRING_MAX_COUNT:	puts("ERROR: too many strings.\n");					break;
-		case ERROR_STRING_CAPACITY:		puts("ERROR: string is too long.\n");				break;
-	}			
-	exit(failure);
+typedef struct {
+	char name[STRING_SIZE];
+	int inlined;
+	int size;
+	int commands[MAX_CELLS_IN_DEFINITION];
+} definition_t;
+
+run_mode_t g_mode;
+char g_word[STRING_SIZE];
+definition_t g_dictionary[MAX_DEFINITIONS_IN_DICTIONARY];
+int g_dictionary_i;
+int g_stack[MAX_STACK_SIZE];
+int g_stack_i;
+
+int whitespace(int c) {
+	return c == EOF || c == ' ' || c == '\n';
 }
 
-void init_string(string_t* str) {
-	if (g_string_count >= STRING_MAX_COUNT)
-		fail(ERROR_STRING_MAX_COUNT);
-	str->chars = g_string_heap + (g_string_count++ * STRING_CAPACITY);
-}
-
-int read_until(string_t* str, char end) {
-	int c;
-	size_t len = 0;
-	while (c = getc(stdin)) {
-		if (c == EOF) {
-			if (feof(stdin))
-				return 1;
-			fail(ERROR_READ_INPUT);
-		}
-		if (end == c)
+int string_equals(char* s1, char* s2) {
+	for (int i = 0; 1; ++i) {
+		if (!s1[i] && !s2[i])
+			return 1;
+		if (s1[i] != s2[i])
 			return 0;
-		if (len >= STRING_CAPACITY)
-			fail(ERROR_STRING_CAPACITY);
-		str->chars[len++] = (char) c;
 	}
 }
 
-void write_string(string_t* str) {
-	puts(str->chars);
+void read_word(void) {
+	size_t i;
+	for (i = 0; i < STRING_CAPACITY; ++i) {
+		int c = getchar();
+		if (whitespace(c))
+			break;
+		g_word[i] = (char) c;
+	}
+	g_word[i] = 0;
+}
+
+definition_t* find_word(char* str) {
+	for (int i = g_dictionary_i; i > 0; --i) {
+		char* defname = g_dictionary[i - 1].name;
+		if (string_equals(str, defname))
+			return &g_dictionary[i - 1];
+	}
+	return 0;
+}
+
+int is_number(char *str) {
+	for (char *c = str; *c != 0; ++c) {
+		if (*c < '0' || *c > '9')
+			return 0;
+	}
+	return 1;
+}
+
+int number(char* str) {
+	int x = 0;
+	for (char *c = str; *c != 0; ++c) {
+		x *= 10;
+		x += *c - '0';
+	}
+	return x;
+}
+
+void print_stack(void) {
+	for (int i = 0; i < g_stack_i; ++i)
+		printf("%d\n", g_stack[i]);
+}
+
+void append(int data) {
+	definition_t def;
+	switch (g_mode) {
+		case EXECUTE:
+			g_stack[g_stack_i++] = data;
+			break;
+		case COMPILE:
+			def = g_dictionary[g_dictionary_i];
+			def.commands[def.size++] = CONSTANT; 
+			def.commands[def.size++] = data;
+			break;
+	}
+}
+
+void append_definition(definition_t* def) {
+	int i = 0;
+	while (i < def->size) {
+		switch (def->commands[i]) {
+			case CONSTANT:
+				append(def->commands[i + 1]);
+				i += 2;
+				break;
+
+			case PLUS:
+				switch (g_mode) {
+					case EXECUTE: {
+						int sum = g_stack[--g_stack_i] + g_stack[--g_stack_i];
+						append(sum);
+						i += 1;
+						break;
+					}
+					case COMPILE:
+						printf("NOT IMPLEMENTED");
+						exit(1);
+						break;	
+				}
+				break;
+
+			case PEEK_STACK: {
+				print_stack();
+				i += 1;
+				break;
+			}
+		}
+	}
+}
+
+void interpret_word(void) {
+	definition_t* def = find_word(g_word);
+	if (def) {
+		append_definition(def);
+	} else if (is_number(g_word)) {
+		append(number(g_word));
+	}
+}
+
+void add_builtin_to_dictionary(char* str, command_t command) {
+	definition_t* def = &g_dictionary[g_dictionary_i++];
+	for (char* c = str; *c != 0; ++c) {
+		def->name[c - str] = *c;
+	}
+	def->inlined = 1;
+	def->size = 1;
+	def->commands[0] = command;
+}
+
+void init_dictionary(void) {
+	add_builtin_to_dictionary(":", BEGIN_DEFINE);
+	add_builtin_to_dictionary(";", END_DEFINE);
+	add_builtin_to_dictionary(">>0", TO_RETURN_STACK);
+	add_builtin_to_dictionary("0>>", FROM_RETURN_STACK);
+	add_builtin_to_dictionary(".", PRINT_STACK);
+	add_builtin_to_dictionary("..", PEEK_STACK);
+	add_builtin_to_dictionary("+", PLUS);
 }
 
 int main(int argc, char **argv) 
 {
-	string_t word;
-	while (1) {	
-		init_string(&word);
-		read_until(&word, '\n');
-		write_string(&word);
+	init_dictionary();
+	while (1) {
+		read_word();
+		interpret_word();
 	}
 }
